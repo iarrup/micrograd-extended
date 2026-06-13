9dba98bdeabb56ba6969d503f1d3c314e929f87c
couch.potato <arup.chowdhary@gmail.com>
2026-06-13 11:16:14 -0700

    cleaned up the variables

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -61,7 +61,8 @@ class Value:
         return out
 
     def sigmoid(self):
-        s = 1 / (1 + math.exp(-self.data))
+        z = self.data
+        s = 1 / (1 + math.exp(-z))
         out = Value(s, (self, ), 'sigmoid')
 
         def _backward():
@@ -71,8 +72,8 @@ class Value:
     
 
     def tanh(self):
-        x = self.data
-        t = (math.exp(x) - math.exp(-x)) / (math.exp(x) + math.exp(-x))
+        z = self.data
+        t = (math.exp(z) - math.exp(-z)) / (math.exp(z) + math.exp(-z))
 
         out = Value(t, (self, ), 'tanh')
 
